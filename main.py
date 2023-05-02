d29d55949222df04266a67254330846b73d830e8
ajaykumaravula <53829369+vishnuvardhan3@users.noreply.github.com>
2023-05-02 19:53:21 +0200

    Adding evn variable

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # main.py
-
+import os
+  
 from fastapi import FastAPI
 
 app = FastAPI()
-
+appenv = os.environ['APPENV']
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {
+        "message": "World",
+        "APPENV": appenv
+        }
